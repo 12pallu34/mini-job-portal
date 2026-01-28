40e259f89da81ed96981e8f7378c0e91c0e5f4d4
pallavi <gdpallavi370@gmail.com>
2026-01-28 23:49:07 +0530

    Remove app.run for Render deployment

--- app.py
+++ app.py
@@ -56,6 +56,5 @@ def apply(job_id):
 
     return render_template("apply.html")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=5000)
+
+
